45b6de702932ca8937442009add82de793749c64
小莫 <xiaomo1433675239@outlook.com>
2022-09-22 14:27:06 +0800

    修改文件路径

---
+++ The Scroll OfTaiwu/ScriptTrainer/UnityGameUI/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace UnityGameUI
+{
+    internal delegate bool d_LoadImage(IntPtr tex, IntPtr data, bool markNonReadable);
+    internal delegate bool parseHTMLString(IntPtr HTMLString, IntPtr result);
+
+    public static class Extensions
+    {
+        // Load Image ICall
+
+        // Convert Hex string to Color32
+        public static Color32 HexToColor(this string hexString)
+        {
+            string tmp = hexString;
+
+            if (tmp.IndexOf('#') != -1)
+                tmp = tmp.Replace("#", "");
+
+            byte r = 0, g = 0, b = 0, a = 0;
+
+            r = Convert.ToByte(tmp.Substring(0, 2), 16);
+            g = Convert.ToByte(tmp.Substring(2, 2), 16);
+            b = Convert.ToByte(tmp.Substring(4, 2), 16);
+            if (tmp.Length == 8)
+                a = Convert.ToByte(tmp.Substring(6, 2), 16);
+
+            return new Color32(r, g, b, a);
+        }
+
+        public static int ConvertToIntDef(this string input, int defaultValue)
+        {
+            int result;
+            if (int.TryParse(input, out result))
+            {
+                return result;
+            }
+            return defaultValue;
+        }
+
+    }
+}
---
+++ The Scroll OfTaiwu/ScriptTrainer/UnityGameUI/UINavigation.cs
@@ -0,0 +1,95 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityGameUI
+{
+    // : MonoBehaviour
+    public static class UINavigation
+    {
+        #region[全局参数]
+
+        public static GameObject panel;   // 父级
+        private static Navigation[] navigations;   // 导航列表
+        private static int elementX;    // 元素X轴坐标
+        private static int elementY;    // 元素Y轴坐标
+        #endregion
+
+        static UINavigation()
+        {
+
+        }
+
+        public static void Initialize(Navigation[] nav, GameObject panel)
+        {
+            navigations = nav;
+            UINavigation.panel = panel;
+
+            elementX = (int)(-panel.GetComponent<RectTransform>().rect.width / 2 + 60);
+            elementY = (int)(panel.GetComponent<RectTransform>().rect.height / 2 - 20);
+
+
+            // 创建导航
+            CreateNavigation();
+        }
+
+        // 创建导航栏
+        public static void CreateNavigation()
+        {
+            foreach (var item in navigations)
+            {
+                string backgroundColor = "#616161FF";
+                Vector3 localPosition = new Vector3(elementX, elementY, 0);
+                GameObject button = UIControls.createUIButton(panel, backgroundColor, item.button, item.SetActive, localPosition);
+                button.GetComponent<RectTransform>().sizeDelta = new Vector2(120, 30);    // 设置按钮大小
+                // 设置字体颜色
+                button.GetComponentInChildren<Text>().color = new Color(255, 255, 255, 1);
+
+
+                item.SetActive(item.show);
+
+                elementY -= 30;
+
+            }
+        }
+
+        public static void SetActive(this Navigation nav)
+        {
+            foreach (Navigation n in navigations)
+            {
+                if (n == nav)
+                {
+                    n.SetActive(true);
+                }
+                else
+                {
+                    n.SetActive(false);
+                }
+            }
+
+        }
+    }
+
+    public class Navigation
+    {
+        public string key;          // 关键字        
+        public string button;       // 按钮
+        public GameObject panel;    // 面板
+        public bool show;           // 是否显示
+
+        public Navigation(string key, string button, GameObject panel, bool show)
+        {
+            this.key = key;
+            this.button = button;
+            this.panel = panel;
+            this.show = show;
+        }
+
+        internal void SetActive(bool v)
+        {
+            show = v;
+            panel.SetActive(v);
+        }
+    }
+}
